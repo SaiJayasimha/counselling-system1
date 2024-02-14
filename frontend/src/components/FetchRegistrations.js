77853125dd7939628ddadff5caea44569e4bf8de
klu2200031004 <2200031004@kluniversity.in>
2024-02-14 16:50:06 +0530

    commit3

---
+++ frontend/src/components/FetchRegistrations.js
@@ -0,0 +1,42 @@
+import React, {useEeffect, useState} from 'react'
+import axios from 'axios'
+const FetchRegistrations = ()=>{
+    const [res,setRes]=useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:8080/retrieve')
+        .then(response=>{
+            setRes(response.data)
+            console.log(Response.data)
+        })
+    },
+    [])
+    return (
+        <div><center><h1>Registrations</h1>
+        <table border={1}>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Role</th>
+                <th>Email</th>
+                <th>Password</th>
+            </tr>
+            {
+                res.map((item,index)=>(
+                   <tr key={index}>
+                    <td>{item._id}
+                    <td>{item.name}</td>
+                    <td>{item.role}</td>
+                    <td>{item.email}</td>
+                    <td>{item.password}</td>                    
+                    </td>
+                   </tr> 
+                ))
+            }
+        </table>
+        </center>
+        </div>
+        
+        
+    )
+
+}
